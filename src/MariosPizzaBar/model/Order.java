fcd4c5b34aaca6a73d576c8e38343728e7a2772e
AmandaHult98 <amhu1000@stud.ek.dk>
2026-03-18 09:28:27 +0100

    Order + OrderSorter

--- src/MariosPizzaBar/model/Order.java
+++ src/MariosPizzaBar/model/Order.java
@@ -1,4 +1,32 @@
 package MariosPizzaBar.model;
 
 public class Order {
+    private int orderNumber;
+    private double time;
+    private Pizza pizza;
+
+    //Constuctor
+    public Order (int orderNumber, double time, Pizza pizza){
+        this.orderNumber = orderNumber;
+        this.time = time;
+        this.pizza = pizza;
+    }
+
+    //Getters
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public String toCSV() {
+        return orderNumber + "," + time + "," + "," + pizza;
+    }
+
 }
--- src/MariosPizzaBar/util/OrderSorter.java
+++ src/MariosPizzaBar/util/OrderSorter.java
@@ -1,4 +1,20 @@
 package MariosPizzaBar.util;
 
+import MariosPizzaBar.model.Order;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class OrderSorter {
+
+    public static void sortByTime(ArrayList<Order> orders) {
+        Collections.sort(orders, Comparator.comparing(Order::getTime));
+    }
+
+    public static void sortByOrderNumber(ArrayList<Order> orders) {
+        Collections.sort(orders, Comparator.comparing(Order::getOrderNumber));
+    }
+
+
 }
