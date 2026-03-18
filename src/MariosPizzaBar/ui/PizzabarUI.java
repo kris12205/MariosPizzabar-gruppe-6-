edc21e22b47d4324bfe718c25e53a35aa2142330
holmganga <siggi.sejr@gmail.com>
2026-03-18 11:52:41 +0100

    Intern funktionalitet sat op. Venter på metoder fra FileHandler. Errorhandling bør også tilføjes

--- src/MariosPizzaBar/ui/PizzabarUI.java
+++ src/MariosPizzaBar/ui/PizzabarUI.java
@@ -2,14 +2,14 @@ package MariosPizzaBar.ui;
 
 import MariosPizzaBar.model.Order;
 import MariosPizzaBar.model.Pizza;
+import MariosPizzaBar.model.Size;
 import MariosPizzaBar.service.FileHandler;
 import MariosPizzaBar.util.OrderSorter;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
+
+import static MariosPizzaBar.model.Size.*;
 
 public class PizzabarUI {
     private static Scanner scanner = new Scanner(System.in);
@@ -18,15 +18,23 @@ public class PizzabarUI {
 
     // skal kalde de andre metoder
     public static void start(){
-        System.out.println("Velkommen til Marios PizzaBar system! Vælg en mulighed " +
+        System.out.println("Velkommen til Marios PizzaBar System! Vælg en mulighed " +
                 "ved at taste et tal fra 1-5");
-        switch (){
+        System.out.println("1. Vis menkort");
+        System.out.println("2. Vis ordreliste");
+        System.out.println("3. Tilføj ordre");
+        System.out.println("4. Færdiggør ordre");
+        System.out.println("5. Vis historik");
+
+        int input = scanner.nextInt();
+
+        switch (input){
             case 1:
                 showMenu();
             case 2:
                 showOrders();
             case 3:
-                addOrder(Scanner scanner);
+                addOrder(scanner);
             case 4:
                 concludeOrder();
             case 5:
@@ -36,10 +44,7 @@ public class PizzabarUI {
 
     // skal printe menukortet fra pizzamenu.csv
     public static void showMenu(){
-        try { // læs og print pizzamenu.csv
-        } catch(Exception e) {
-            // gå til ErrorHandler
-        }
+        fileHandler.loadPizzaMenu();
     }
 
     // skal printe orders
@@ -62,25 +67,50 @@ public class PizzabarUI {
 
     // tilføjer order til orders
     private static void addOrder(Scanner scanner){
-        System.out.println("Ordernummer?");
+
+        System.out.println("Ordrenummer?");
         int orderNumber = scanner.nextInt();
+
         System.out.println("Tidspunkt?");
         int pizzaTime = scanner.nextInt();
+
         System.out.println("Pizzanummer?");
         int pizzaNumber = scanner.nextInt();
+        // skal kalde en metode i FileHandler der opretter en pizza baseret på pizzaNumber
+        //Pizza pizza = fileHandler.readMenu(pizzaNumber);
 
-        // opretter en pizza baseret på pizzaNumber
+        System.out.println("Størrelse?");
+        String answer = scanner.nextLine();
+        if (Objects.equals(answer.toLowerCase(), "normal")){
+            Size size = NORMAL;
+        } else if (Objects.equals(answer.toLowerCase(), "kids") ||
+                    Objects.equals(answer.toLowerCase(), "kid") ||
+                    Objects.equals(answer.toLowerCase(), "small")){
+            Size size = KIDS;
+        } else if (Objects.equals(answer.toLowerCase(), "family") ||
+                    Objects.equals(answer.toLowerCase(), "large")){
+            Size size = FAMILY;
+        } else {
+            System.out.println("Forkert input. Prøv igen.");
+        }
 
-        orders.add(new Order(orderNumber,pizzaTime,pizza));
+        //orders.add(new Order(orderNumber,pizzaTime,pizza));
 
     }
 
     // fjerner order fra orders og tilføjer til Historik.csv
-    private static void concludeOrder(Order order){
-        // skal kalde metode i FileHandler der tilføjer order til historik.csv
-        orders.remove(order);
+    private static void concludeOrder(){
+        showOrders(); // printer ordreliste som referencepunkt
+
+        System.out.println("Hvilke order vil du færdiggøre?");
+        int orderNumber = scanner.nextInt();
+
+        // skal kalde metode i FileHandler der tilføjer order til historik.csv og fjerne fra ArrayList
+       // fileHandler.concludeOrder(orderNumber);
     }
 
     // skal læse og printe historik.csv
-    private static void showHistory(){}
+    private static void showHistory(){
+        // fileHandler.showHistory();
+    }
 }
