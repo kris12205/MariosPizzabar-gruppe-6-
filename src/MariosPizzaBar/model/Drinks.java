dce0f856c27097a69726fcf142819d0e22127312
plattine <loghindan@gmail.com>
2026-03-24 10:27:19 +0100

    ny Drinks class

---
+++ src/MariosPizzaBar/model/Drinks.java
@@ -0,0 +1,11 @@
+package MariosPizzaBar.model;
+
+public class Drinks {
+        static String[] drinks = {"Coca Cola Zero", "Coca Cola", "Fanta", "Faxe Kondi", "Tuborg Classic", "Carlsberg"};
+
+        public static void printDrinks() {
+        System.out.println(String.join("\n", drinks));
+
+
+    }
+}
