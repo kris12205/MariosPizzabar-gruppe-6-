5ba892f090e657048644c44963e4b24e9aca86a9
plattine <loghindan@gmail.com>
2026-03-25 09:10:36 +0100

    ny refactor FileHandlerHistory class

---
+++ src/MariosPizzaBar/service/FileHandlerHistory.java
@@ -0,0 +1,66 @@
+package MariosPizzaBar.service;
+import MariosPizzaBar.model.*;
+import java.io.*;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import MariosPizzaBar.util.ErrorHandler;
+
+
+
+public class FileHandlerHistory {
+
+    private static final String HISTORIK = "src/MariosPizzaBar/Historik.csv";
+    private static ArrayList<Order> pizzaHistory = new ArrayList<>();
+
+
+    public ArrayList<Order> getPizzaHistory() {
+        return pizzaHistory;
+    }
+
+    public void showHistory() {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(HISTORIK))) {
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+
+            }
+            reader.close();
+
+
+        } catch (Exception e) {
+            ErrorHandler.handleErrors(e);
+            ErrorHandler.handlefileErrors(e);
+            ErrorHandler.handleArrayException(e);
+        }
+        //Måske udregne statistikken her?
+
+
+    }
+
+    //Går igennem historik ArrayListen og skriver den til historik CSV filen
+    public static void writeToFileHistory() { //Har fjernet at metoden tager imod en ordre
+        try {
+            FileWriter fileWriter = new FileWriter(HISTORIK);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            for (Order o : pizzaHistory) {
+                bufferedWriter.write(o.toCSV());
+                bufferedWriter.newLine();
+            }
+
+            bufferedWriter.close();
+
+        } catch (Exception e) {
+            ErrorHandler.handleErrors(e);
+            ErrorHandler.handlefileErrors(e);
+            ErrorHandler.handleArrayException(e);
+        }
+
+    }
+
+
+
+}
