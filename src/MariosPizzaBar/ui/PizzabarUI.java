209bd025864be8a6fc66d0b23768abbeb1078773
holmganga <siggi.sejr@gmail.com>
2026-03-25 09:53:12 +0100

    UI refactored. OrderHandler har nu ansvar for ordre metoderne

--- src/MariosPizzaBar/ui/PizzabarUI.java
+++ src/MariosPizzaBar/ui/PizzabarUI.java
@@ -3,7 +3,7 @@ package MariosPizzaBar.ui;
 import MariosPizzaBar.model.Order;
 import MariosPizzaBar.model.Pizza;
 import MariosPizzaBar.model.Size;
-import MariosPizzaBar.service.FileHandler;
+import MariosPizzaBar.service.*;
 import MariosPizzaBar.util.ErrorHandler;
 import MariosPizzaBar.util.OrderSorter;
 import MariosPizzaBar.model.*;
@@ -13,12 +13,13 @@ import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 import static MariosPizzaBar.model.Size.*;
+import static MariosPizzaBar.service.OrderHandler.*;
 import static MariosPizzaBar.service.Stats.Stats;
 
 public class PizzabarUI {
     private static Scanner scanner = new Scanner(System.in);
-    private static ArrayList<Order> orders = new ArrayList<>();
-    private static FileHandler fileHandler = new FileHandler();
+    private static FileHandlerMenu fileHandlerMenu = new FileHandlerMenu();
+    private static FileHandlerHistory fileHandlerHistory = new FileHandlerHistory();
 
     // skal kalde de andre metoder
     public static void start() {
@@ -27,9 +28,10 @@ public class PizzabarUI {
         boolean running = true;
 
         //Indlæser Pizza menuen
-        fileHandler.loadPizzaMenu();
+        fileHandlerMenu.loadPizzaMenu();
+
         //Indlæser bestillingslisten
-        orders = fileHandler.loadOrderList();
+        getOrders();
         showOrders();
 
         while (running) {
@@ -50,8 +52,8 @@ public class PizzabarUI {
                         showOrders();
                         break;
                     case 4:
-                        addOrder(scanner);
-                        scanner.nextLine();
+                        addOrder();
+                        // scanner.nextLine();
                         break;
                     case 5:
                         sortOrderList(scanner);
@@ -59,7 +61,7 @@ public class PizzabarUI {
                         break;
                     case 6:
                         concludeOrder();
-                        scanner.nextLine();
+                        // scanner.nextLine();
                         break;
                     case 7:
                         showHistory();
@@ -84,155 +86,28 @@ public class PizzabarUI {
 
     // skal printe menukortet fra pizzamenu.csv
     public static void showMenu() {
-        for (Pizza pizza : fileHandler.getPizzas()) {
+        for (Pizza pizza : fileHandlerMenu.getPizzas()) {
             System.out.print(pizza + "\n");
         }
     }
 
-    // skal printe orders
-    public static void showOrders() {
-        String allOrders = "";
-
-        if (orders.isEmpty()) {
-
-            System.out.println("Ordrelisten er tom.");
-
-        } else {
-
-            for (Order order : orders) {
-                allOrders = allOrders.concat(order.toCSV() + "\n");
-
-            }
-            System.out.println(allOrders);
-        }
-    }
-
-    // tilføjer order til orders
-    private static void addOrder(Scanner scanner) {
-
-        System.out.println("Hvor mange pizzaer vil du tilføje?");
-
-        int pizzaAntal = Integer.parseInt(scanner.nextLine());
-
-        for (int i = 0; i < pizzaAntal; i++) {
-            System.out.println("\nPizza #" + (i + 1));
-
-            System.out.println("Pizzanummer?");
-            int pizzaNumber = scanner.nextInt();
-            Pizza newOrder = fileHandler.findPizza(pizzaNumber);
-            scanner.nextLine();
-
-            System.out.println("Er størrelsen 'normal', 'kids' eller 'large'?");
-            Size size = null;
-
-            try {
-                size = Size.valueOf(scanner.nextLine().toUpperCase());
-            } catch (Exception e) {
-                ErrorHandler.handleErrors(e);
-                ErrorHandler.handleNullException(e);
-                ErrorHandler.handleInputErrors(e);
-                return;
-            }
-
-            fileHandler.addOrder(new Order(newOrder, size));
-            addCustomer(newOrder);
-            System.out.println("\nPizza #" + (i + 1) + "er tilføjet");
-
-    }
-
-    }
-
-    public static void addCustomer(Pizza pizza){
-
-        System.out.println("Hvilken slags kunde bestiller?\n1. Normal \n2. VIP\n3. Medarbejder");
-        int input = scanner.nextInt();
-        System.out.print("Indtast kundenummer: ");
-        int customerNumber = scanner.nextInt();
-
-        //Udregner pris
-        switch (input) {
-            case 1:
-                NormalCustomer nc = new NormalCustomer(customerNumber);
-                System.out.println(nc.discount(pizza.getPrice()));
-                break;
-            case 2:
-                VIPCustomer vip = new VIPCustomer(customerNumber);
-                System.out.println(vip.discount(pizza.getPrice()));
-                break;
-            case 3:
-                EmployeeCustomer ec = new EmployeeCustomer(customerNumber);
-                System.out.println(ec.discount(pizza.getPrice()));
-                break;
-            default:
-                System.out.println("Ukendt input.");
-        }
-
-        }
-
-    //Sorter ordrelisten
-    public static void sortOrderList(Scanner scan) {
-        System.out.println("Hvordan vil du sorter ordrelisten?\n1. Efter tid\n2. Alfabetisk\n3. Efter pizza navn");
-        int input = scan.nextInt();
-
-        switch (input) {
-            case 1:
-                OrderSorter.sortByTime(orders);
-                System.out.println(orders);
-                break;
-            case 2:
-                OrderSorter.sortByOrderNumber(orders);
-                System.out.println(orders);
-                break;
-            case 3:
-                OrderSorter.sortByOrderName(orders);
-                System.out.println(orders);
-                break;
-            default:
-                System.out.println("Ukendt input");
-        }
-    }
-
-    // fjerner order fra orders og tilføjer til Historik.csv
-    private static void concludeOrder() {
-        System.out.println("Hvor mange ordrer vil du færdiggøre?");
-
-        int pizzaAntal = Integer.parseInt(scanner.nextLine());
-
-        for (int i = 0; i < pizzaAntal; i++) {
-            if (orders.isEmpty()) {
-                System.out.println("Ordrelisten er tom.");
-            } else {
-                showOrders(); //Printer ordreliste som referencepunkt
-
-                System.out.println("Hvilke order vil du færdiggøre?");
-                int orderNumber = scanner.nextInt();
-                ArrayList<Order> updatedOrderList = fileHandler.removeOrder(orderNumber);
-
-                if (updatedOrderList == null) {
-                    System.out.println("Denne ordre eksisterer ikke.");
-                } else {
-                    System.out.println("Ordre nr." + orderNumber + " er færdiggjort.");
-                }
-            }
-        }
-    }
 
     // skal læse og printe historik.csv
     private static void showHistory() {
-        if(fileHandler.getPizzaHistory().isEmpty()) {
+        if(fileHandlerHistory.getPizzaHistory().isEmpty()) {
             System.out.println("Historikken er tom");
         } else {
-            fileHandler.showHistory();
+            fileHandlerHistory.showHistory();
         }
 
     }
 
     //Kalder stats metode
     private static void showStats() {
-        if(fileHandler.getPizzaHistory().isEmpty()) {
+        if(fileHandlerHistory.getPizzaHistory().isEmpty()) {
             System.out.println("Historikken er tom");
         } else {
-            System.out.println(Stats(fileHandler.getPizzaHistory()));
+            System.out.println(Stats(fileHandlerHistory.getPizzaHistory()));
         }
 
     }
