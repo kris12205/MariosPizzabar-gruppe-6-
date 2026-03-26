7395414b490d0961455cd1ceb175776632d43ce1
kris12205 <mini429h@gmail.com>
2026-03-26 09:46:59 +0100

    Added loadHistory metode i FileHandlerHistory og fikset error i PizzabarUI

--- src/MariosPizzaBar/ui/PizzabarUI.java
+++ src/MariosPizzaBar/ui/PizzabarUI.java
@@ -35,6 +35,7 @@ public class PizzabarUI {
 
         //Indlæser Pizza menuen
         fileHandlerMenu.loadPizzaMenu();
+        fileHandlerHistory.loadHistory();
 
         //Indlæser bestillingslisten
         getOrders();
@@ -81,11 +82,10 @@ public class PizzabarUI {
                         System.out.println(Color.YELLOW + "Tak for nu." + Color.RESET);
                         break;
                     default:
-                        System.out.println(Color.RED + "Uventet fejl. Tast et tal mellem 1-9." + Color.RESET);
+                        System.out.println(Color.RED + "Fejl. Tast et tal mellem 1-9." + Color.RESET);
 
                 }
             } catch (Exception e){
-               ErrorHandler.handleErrors(e);
                ErrorHandler.handleInputErrors(e);
             }
         }
