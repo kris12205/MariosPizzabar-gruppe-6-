7a60948e45a84d1c243b752a0a65274016c9f1cb
plattine <loghindan@gmail.com>
2026-03-27 12:56:52 +0100

    fjernet color kode fra FileHandlerHistory.

--- src/MariosPizzaBar/service/FileHandlerHistory.java
+++ src/MariosPizzaBar/service/FileHandlerHistory.java
@@ -1,5 +1,6 @@
 package MariosPizzaBar.service;
 import MariosPizzaBar.model.*;
+import MariosPizzaBar.util.Color;
 import java.io.*;
 
 import java.util.ArrayList;
@@ -9,11 +10,6 @@ import MariosPizzaBar.util.ErrorHandler;
 
 
 public class FileHandlerHistory {
-    // definerer farver i ANSI
-    private static String RED = "\u001B[31m";
-    private static String GREEN = "\u001B[32m";
-    private static String YELLOW = "\u001B[33m";
-    private static String RESET = "\u001B[0m";
 
     private static final String HISTORIK = "src/MariosPizzaBar/Historik.csv";
     private static ArrayList<Order> pizzaHistory = new ArrayList<>();
@@ -30,7 +26,7 @@ public class FileHandlerHistory {
             String line;
 
             while ((line = reader.readLine()) != null) {
-                System.out.println(GREEN + line + RESET);
+                System.out.println(Color.GREEN + line + Color.RESET);
 
             }
             reader.close();
